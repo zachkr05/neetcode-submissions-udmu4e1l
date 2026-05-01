723c1fdc3f3bede1df94e804a8b592e5b482308c
Zachariah K. Risheq <74123318+zachkr05@users.noreply.github.com>
2026-04-30 23:39:40 -0400

    Add: depth-of-binary-tree - submission-2

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-2.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+
+int _soln; 
+private:
+    int dfs(TreeNode* root){
+        if(!root){
+            return 0;
+        }
+
+        return 1 + max(dfs(root->left), dfs(root->right));
+    }
+public:
+    int maxDepth(TreeNode* root) {
+        return dfs(root);
+    }
+};
